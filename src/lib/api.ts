769c94ec5f81d034d6e0e58d6fa15482807bf1d9
ecode <nikollasohta2014@gmail.com>
2026-04-20 05:40:30 -0300

    feat: add axios api client with bearer and 401 interceptors

---
+++ src/lib/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:3000',
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('admin_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+api.interceptors.response.use(
+  (res) => res,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('admin_token')
+      window.dispatchEvent(new CustomEvent('auth:logout'))
+    }
+    return Promise.reject(error)
+  },
+)
